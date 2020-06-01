16536b0ed56b7263e8ecc31835d71da671f90d0e
yang7021 <1097085674@qq.com>
2020-06-01 14:18:11 +0800

    feat: test pre commit 1

--- app/controller/home.js
+++ app/controller/home.js
@@ -5,7 +5,7 @@ const Controller = require('egg').Controller
 class HomeController extends Controller {
   async index() {
     const { ctx } = this;
-    console.log('xxxxs')
+    console.log('xxxxs');
     ctx.body = 'hi, egg'
   }
 }
